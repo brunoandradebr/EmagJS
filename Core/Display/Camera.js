88297183317c86f3cd6d38e8398b26e54fb78298
Bruno Andrade <bruno.faria.andrade@gmail.com>
2018-06-16 02:42:18 -0300

    Added zoom in and out

--- Core/Display/Camera.js
+++ Core/Display/Camera.js
@@ -16,10 +16,16 @@ class Camera {
         this.height = height
         this.offsetHorizontal = 40
         this.offsetVertical = 0
-        this.speed = 0.1
-        this.zoom = 1
+        this.speed = this.originalSpeed = 0.1
+        this.zoomScale = 1
 
         this.target = null
+
+        this.zoomInAnimation = new Tween(this)
+        this.zoomInAnimation.animate({ zoomScale: 1 }, 500, 0, 'cubicOut')
+        this.zoomOutAnimation = new Tween(this)
+        this.zoomOutAnimation.animate({ zoomScale: 1 }, 500, 0, 'cubicOut')
+
     }
 
     /**
@@ -32,8 +38,8 @@ class Camera {
         let centerX = this.x + this.width * 0.5
         let centerY = this.y + this.height * 0.5
 
-        let targetCenterX = (this.target.position.x * this.zoom) + (this.target.width * 0.5)
-        let targetCenterY = (this.target.position.y * this.zoom) + (this.target.height * 0.5)
+        let targetCenterX = (this.target.position.x * this.zoomScale) + (this.target.width * 0.5)
+        let targetCenterY = (this.target.position.y * this.zoomScale) + (this.target.height * 0.5)
 
         let targetDirectionX = this.target.direction ? this.target.direction.x : 1
         let targetDirectionY = this.target.direction ? this.target.direction.y : 1
@@ -49,6 +55,18 @@ class Camera {
         this.y = this.y | 0
     }
 
+    zoomIn() {
+        // set camera speed to 1 to zoom nicely
+        // double scene height
+        // ver o metodo que reseta a animacao...
+        this.zoomInAnimation.play()
+    }
+
+    zoomOut() {
+        this.zoomOutAnimation.updateAnimation(0, this.zoomScale, 1)
+        this.zoomOutAnimation.play()
+    }
+
     /**
      * 
      * @param {number} minX 
@@ -60,8 +78,8 @@ class Camera {
      */
     limitArea(minX, minY, maxX, maxY) {
 
-        if (this.x + this.width > maxX * this.zoom) {
-            this.x = maxX * this.zoom - this.width
+        if (this.x + this.width > maxX * this.zoomScale) {
+            this.x = maxX * this.zoomScale - this.width
         }
 
         if (this.x < 0) {
