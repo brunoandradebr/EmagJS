b7018ce7eda58a6b3e4e27aa90daf03abe495998
Bruno Andrade <bruno.faria.andrade@gmail.com>
2018-07-11 23:56:11 -0300

    Documented ObjectPool class

--- Core/Common/Misc/ObjectPool.js
+++ Core/Common/Misc/ObjectPool.js
@@ -1,31 +1,77 @@
+/**
+ * @author Bruno Andrade <bruno.faria.andrade@gmail.com>
+ */
+
+/**
+ * Object pool pattern
+ */
 class ObjectPool {
 
+
+    /**
+     * 
+     * @param {function} initializer
+     * @param {function} reseter
+     */
     constructor(initializer = () => { }, reseter = () => { }) {
 
+        /**
+         * Method that describes what to be created
+         * 
+         * @type {function}
+         */
         this.initializer = initializer
+
+        /**
+         * Method that reset created object
+         * 
+         * @type {function}
+         */
         this.reseter = reseter
 
-        this.busy = [initializer()]
+        /**
+         * Container that holds pooled objects
+         * 
+         * @type {array}
+         */
+        this.inUse = [initializer()]
+
+        /**
+         * Array that holds object's index that are free to be used
+         * 
+         * @type {array}
+         */
         this.free = [0]
 
     }
 
-    createObject() {
+    /**
+     * Allocate or get a pooled object
+     * 
+     * @return {void}
+     */
+    create() {
 
+        // all pooled objects are been used
         if (!this.free.length) {
 
-            let oldLength = this.busy.length
+            // cache current inUse array length
+            let oldLength = this.inUse.length
 
-            this.busy.length = this.busy.length + 4
+            // update inUse length
+            this.inUse.length = this.inUse.length + 2
 
-            for (let i = oldLength; i < this.busy.length; i++) {
-                this.busy[i] = this.initializer()
+            // allocate new objects to be used
+            for (let i = oldLength; i < this.inUse.length; i++) {
+                this.inUse[i] = this.initializer()
                 this.free.push(i)
             }
         }
 
+        // get a free object from pool
         let index = this.free.pop()
-        let object = this.busy[index]
+        let object = this.inUse[index]
+        // reset object state
         object = this._resetObject(object)
         object.index = index
 
@@ -33,12 +79,28 @@ class ObjectPool {
 
     }
 
-    _resetObject(object) {
-        return this.reseter(object)
+    /**
+     * Destroy
+     * 
+     * Send object's index to free array
+     * 
+     * @param {object} object 
+     * 
+     * @return {void}
+     */
+    destroy(object) {
+        this.free.push(object.index)
     }
 
-    destroyObject(object) {
-        this.free.push(object.index)
+    /**
+     * private _resetObject
+     * 
+     * @param {object} object
+     * 
+     * @return {object} 
+     */
+    _resetObject(object) {
+        return this.reseter(object)
     }
 
 }
