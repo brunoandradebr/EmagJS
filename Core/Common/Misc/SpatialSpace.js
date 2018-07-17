04f81fc93da925b80328b5c5e2aede87c8d94c31
= <bruno@hostnet.com.br>
2018-07-17 19:04:54 -0300

    Improved spatial partition

--- Core/Common/Misc/SpatialSpace.js
+++ Core/Common/Misc/SpatialSpace.js
@@ -10,7 +10,7 @@ class SpatialSpace {
 
     /**
      * 
-     * @param {integer} rows   - Number of rows
+     * @param {integer} rows    - Number of rows
      * @param {integer} columns - Number of columns
      * @param {number}  width   - Max width of the spatial area 
      * @param {number}  height  - Max height of the spatial area
@@ -20,22 +20,22 @@ class SpatialSpace {
         /**
          * @type {integer}
          */
-        this.rows = rows || 3
+        this.rows = rows
 
         /**
          * @type {integer}
          */
-        this.columns = columns || 2
+        this.columns = columns
 
         /**
          * @type {number}
          */
-        this.width = width || 1
+        this.width = width
 
         /**
          * @type {number}
          */
-        this.height = height || 1
+        this.height = height
 
         /**
          * @type {number}
@@ -86,9 +86,16 @@ class SpatialSpace {
         /**
          * debug only
          * 
-         * @type {array}
+         * @type {array<integer>}
          */
         this.debugBlocks = []
+
+        /**
+         * Keep track of colliding debug blocks
+         * 
+         * @type {array<object>}
+         */
+        this.collidingDebugAreas = []
 
     }
 
@@ -123,28 +130,105 @@ class SpatialSpace {
     }
 
     /**
-     * Add a new object to an area based on it's position
+     * Add a new object to an area based on it's bounding box points and it's center
      * 
-     * @param {*} objects - Any interactive object that has a position vector
+     *    *-----*
+     *    |  *  |
+     *    *-----*
+     * 
+     * @param {array<*>} objects - Array with any interactive object that has a position vector
      * 
      * @return {void} 
      */
-    addObjects(objects) {
+    addObjects(objects, boundingBox = false) {
 
         // clear all before add objects
         this.clearAreas()
 
         // for each object
-        objects.map((object) => {
+        objects.map((object, i) => {
 
-            // finds it's position index in 2D array
-            let x = (object.position.x * this.iBlockWidth) | 0
-            let y = (object.position.y * this.iBlockHeight) | 0
+            // id to filter repeated object inside the same area
+            object.spatialID = i
 
-            // add object
-            if (this.areas[y]) {
-                if (this.areas[x]) {
-                    this.areas[x][y].push(object)
+            let objectStructure
+
+            if (boundingBox) {
+                objectStructure = object.getBoundingBox()
+            } else {
+                objectStructure = {}
+                objectStructure.width = object.width
+                objectStructure.height = object.height
+                objectStructure.centerX = object.position.x
+                objectStructure.centerY = object.position.y
+            }
+
+            // half width and height
+            let objectHalfWidth = objectStructure.width * 0.5
+            let objectHalfHeight = objectStructure.height * 0.5
+            // center
+            let objectCenterX = objectStructure.centerX
+            let objectCenterY = objectStructure.centerY
+            // upper left
+            let objectUpperLeftX = objectCenterX - objectHalfWidth
+            let objectUpperLeftY = objectCenterY - objectHalfHeight
+            // upper right
+            let objectUpperRightX = objectCenterX + objectHalfWidth
+            let objectUpperRightY = objectCenterY - objectHalfHeight
+            // bottom left
+            let objectBottomLeftX = objectCenterX - objectHalfWidth
+            let objectBottomLeftY = objectCenterY + objectHalfHeight
+            // bottom right
+            let objectBottomRightX = objectCenterX + objectHalfWidth
+            let objectBottomRightY = objectCenterY + objectHalfHeight
+
+            // add object based on it's center position
+            let centerIndexX = (objectCenterX * this.iBlockWidth) | 0
+            let centerIndexY = (objectCenterY * this.iBlockHeight) | 0
+
+            if (this.areas[centerIndexY]) {
+                if (this.areas[centerIndexX]) {
+                    this.areas[centerIndexX][centerIndexY].push(object)
+                }
+            }
+
+            // add object based on it's upper left position
+            let upperLeftIndexX = (objectUpperLeftX * this.iBlockWidth) | 0
+            let upperLeftIndexY = (objectUpperLeftY * this.iBlockHeight) | 0
+
+            if (this.areas[upperLeftIndexY]) {
+                if (this.areas[upperLeftIndexX]) {
+                    this.areas[upperLeftIndexX][upperLeftIndexY].push(object)
+                }
+            }
+
+            // add object based on it's upper right position
+            let upperRightIndexX = (objectUpperRightX * this.iBlockWidth) | 0
+            let upperRightIndexY = (objectUpperRightY * this.iBlockHeight) | 0
+
+            if (this.areas[upperRightIndexY]) {
+                if (this.areas[upperRightIndexX]) {
+                    this.areas[upperRightIndexX][upperRightIndexY].push(object)
+                }
+            }
+
+            // add object based on it's bottom left position
+            let bottomLeftIndexX = (objectBottomLeftX * this.iBlockWidth) | 0
+            let bottomLeftIndexY = (objectBottomLeftY * this.iBlockHeight) | 0
+
+            if (this.areas[bottomLeftIndexY]) {
+                if (this.areas[bottomLeftIndexX]) {
+                    this.areas[bottomLeftIndexX][bottomLeftIndexY].push(object)
+                }
+            }
+
+            // add object based on it's bottom right position
+            let bottomRightIndexX = (objectBottomRightX * this.iBlockWidth) | 0
+            let bottomRightIndexY = (objectBottomRightY * this.iBlockHeight) | 0
+
+            if (this.areas[bottomRightIndexY]) {
+                if (this.areas[bottomRightIndexX]) {
+                    this.areas[bottomRightIndexX][bottomRightIndexY].push(object)
                 }
             }
 
@@ -179,16 +263,54 @@ class SpatialSpace {
         for (let x = 0; x < this.areas.length; x++) {
             for (let y = 0; y < this.areas.length; y++) {
 
-                let mainArea = this.areas[x][y]
+                let area = this.areas[x][y]
 
-                if (mainArea.length > 1) {
-                    collisionAreas.push(mainArea)
+                // array to keep not repeated objects
+                let filteredObjects = [area[0]]
+
+                // filter repeated objects inside area
+                area.map((object) => {
+                    if (filteredObjects[filteredObjects.length - 1].spatialID != object.spatialID) {
+                        filteredObjects.push(object)
+                    }
+                })
+
+                // if two or more objects inside area, push area to possible colliding area
+                if (filteredObjects.length > 1) {
+
+                    collisionAreas.push(filteredObjects)
+
+                    // debug only - debug area sprite to highlight
+                    this.collidingDebugAreas.push({ x: x, y: y })
                 }
             }
         }
 
+        // return all areas that possible collisions will happen
         return collisionAreas
 
+    }
+
+    /**
+     * get total collision pair checks
+     * 
+     * @return {interger}
+     */
+    get totalCollision() {
+
+        let total = 0
+
+        this.collisionAreas.map((area) => {
+
+            for (let i = 0; i < area.length; i++) {
+                for (let j = i + 1; j < area.length; j++) {
+                    total++
+                }
+            }
+
+        })
+
+        return total
     }
 
     /**
@@ -201,8 +323,8 @@ class SpatialSpace {
      */
     getDebugBlock(x, y) {
 
-        let xIndex = mouse.x * this.iBlockWidth | 0
-        let yIndex = mouse.y * this.iBlockHeight | 0
+        let xIndex = x
+        let yIndex = y
 
         let blockIndex = xIndex + (yIndex * this.rows)
 
@@ -233,10 +355,24 @@ class SpatialSpace {
 
         } else {
 
+            // reset initial block line color
+            this.debugBlocks.map((debugBlock) => {
+                debugBlock.lineColor = 'rgba(0,120,255,0.2)'
+            })
+
+            // highlight colliding areas
+            this.collidingDebugAreas.map((collidingDebugBlock) => {
+                let area = this.getDebugBlock(collidingDebugBlock.x, collidingDebugBlock.y)
+                area.lineColor = 'red'
+            })
+
             // draw all area blocks
             this.debugBlocks.map((debugBlock) => {
                 debugBlock.draw(graphics)
             })
+
+            // clear colliding areas
+            this.collidingDebugAreas.length = 0
 
         }
     }
