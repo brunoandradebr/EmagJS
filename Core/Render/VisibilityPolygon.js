99315717221e8dde3d3883c4a5e1aa53d4870907
= <bruno@hostnet.com.br>
2019-01-09 19:44:24 -0200

    Added contains method to visibility polygon

--- Core/Render/VisibilityPolygon.js
+++ Core/Render/VisibilityPolygon.js
@@ -2,10 +2,33 @@ class VisibilityPolygon {
 
     constructor(position = new Vector(DEVICE_CENTER_X, DEVICE_CENTER_Y), fillColor = 'rgba(255, 255, 255, 0.7)', collisionHandler = new CollisionHandler()) {
 
+        /**
+         * light color
+         * 
+         * @type {string}
+         */
         this.fillColor = fillColor
-        this.colorRadius0 = 0
-        this.colorRadius1 = 800
+
+        /**
+         * shadow color
+         * 
+         * @type {string}
+         */
         this.shadowColor = 'rgba(0, 0, 0, 0.6)'
+
+        /**
+         * inside color radius
+         * 
+         * @type {number}
+         */
+        this.colorRadius0 = 0
+
+        /**
+         * outside color radius
+         * 
+         * @type {number}
+         */
+        this.colorRadius1 = 800
 
         /**
          * polygon center
@@ -30,6 +53,9 @@ class VisibilityPolygon {
          */
         this.rays = []
 
+        this.tmpPointToSegmentLine = new Line(new Vector, new Vector)
+        this.tmpSegmentLine = new Line(new Vector, new Vector)
+
         /**
          * Collision handler object to check ray to polygon intersection
          * 
@@ -48,6 +74,58 @@ class VisibilityPolygon {
      */
     addPolygon(polygon) {
         this.polygons.push(polygon)
+    }
+
+    /**
+     * Checks if a point is inside visibility polygon 
+     * 
+     * @type {EmagJS.Core.Math.Vector} point 
+     * 
+     * @return {bool}
+     */
+    contains(point) {
+
+        // creates a line from point to light center
+        // object <- ☺ ------------ o -> light center
+        //
+        // update tmp point to segment line start
+        this.tmpPointToSegmentLine.start.x = this.position.x
+        this.tmpPointToSegmentLine.start.y = this.position.y
+        // update tmp point to segment line end
+        this.tmpPointToSegmentLine.end.x = point.x
+        this.tmpPointToSegmentLine.end.y = point.y
+
+        // get rays end points
+        let endPoints = []
+        this.rays.map((ray) => {
+            endPoints.push(ray.end)
+        })
+
+        let inside = true
+
+        // for each light shape segment
+        for (let i = 0; i < endPoints.length; i++) {
+
+            // get segments points
+            let A = endPoints[i]
+            let B = endPoints[i + 1] ? endPoints[i + 1] : endPoints[0]
+
+            // update tmp segment line start
+            this.tmpSegmentLine.start.x = A.x
+            this.tmpSegmentLine.start.y = A.y
+            // update tmp segment line end
+            this.tmpSegmentLine.end.x = B.x
+            this.tmpSegmentLine.end.y = B.y
+
+            // if point to center line intersects with any light shape segment
+            if (this.collisionHandler.check(this.tmpSegmentLine, this.tmpPointToSegmentLine)) {
+                inside = false
+                break
+            }
+
+        }
+
+        return inside
     }
 
     /**
@@ -110,8 +188,6 @@ class VisibilityPolygon {
      * @return {void}
      */
     draw(graphics) {
-
-        //this.rays.map((ray) => ray.draw(graphics))
 
         this.polygons[0].fillColor = this.shadowColor
         this.polygons[0].draw(graphics)
