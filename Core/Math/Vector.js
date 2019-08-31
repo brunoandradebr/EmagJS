952cee96ebb35fafbeee0884e2d0517ecdb65c0e
Bruno Andrade <bruno.faria.andrade@gmail.com>
2019-08-31 19:41:52 -0300

    vector debug with an arrow direction

--- Core/Math/Vector.js
+++ Core/Math/Vector.js
@@ -390,12 +390,24 @@ class Vector {
 
         Object.assign(option, definition);
 
+        let endX = option.center.x + this.x
+        let endY = option.center.y + this.y
+
+        // aux lines to create an arrow
+        let leftLine = Vector.fromAngle((this.angle * toDegree) + 130, this.x * this.x + this.y * this.y > 10 * 10 ? 10 : 5)
+        let rightLine = Vector.fromAngle((this.angle * toDegree) - 130, this.x * this.x + this.y * this.y > 10 * 10 ? 10 : 5)
+
         graphics.strokeStyle = option.lineColor;
         graphics.lineWidth = option.lineWidth;
 
         graphics.beginPath();
         graphics.moveTo(option.center.x, option.center.y);
-        graphics.lineTo(option.center.x + this.x, option.center.y + this.y);
+        graphics.lineTo(endX, endY);
+        // draw arrow
+        graphics.moveTo(endX, endY);
+        graphics.lineTo(endX + leftLine.x, endY + leftLine.y);
+        graphics.moveTo(endX, endY);
+        graphics.lineTo(endX + rightLine.x, endY + rightLine.y);
         graphics.stroke();
 
     }
