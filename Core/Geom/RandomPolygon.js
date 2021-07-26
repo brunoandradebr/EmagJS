db7c8b2fd38ceb40dbaad2f1e6b30c5221a1521f
Bruno Andrade <bruno.faria.andrade@gmail.com>
2021-07-25 23:16:20 -0300

    Changed random polygon generation

--- Core/Geom/RandomPolygon.js
+++ Core/Geom/RandomPolygon.js
@@ -6,23 +6,37 @@
 /**
  * Random polygon
  * 
+ * @param {number} nVertices number of points
+ * 
  * @extends EmagJS.Core.Geom.Polygon
  */
 class RandomPolygon extends Polygon {
 
-    constructor() {
+    constructor(nVertices = 9) {
 
-        super();
+        super()
+
+        const points = []
+
+        for (let i = 0; i < nVertices; i++) {
+
+            const r = random(Date.now())
+
+            const x = Math.cos(r + i)
+            const y = Math.sin(r + i)
+
+            const point = new Vector(x, y, 0)
+
+            points.push(point)
+
+        }
+
+        points.sort((a, b) => b.angle - a.angle)
 
         /**
          * @type {array<EmagJS.Core.Math.Vector>}
          */
-        this.points = [
-            new Vector(Math.random(), Math.random()),
-            new Vector(-Math.random(), Math.random()),
-            new Vector(-Math.random(), -Math.random()),
-            new Vector(Math.random(), -Math.random()),
-        ];
+        this.points = points
 
     }
 
